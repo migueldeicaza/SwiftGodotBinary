5b546399f43eedcb80669b507399c3363171710e
Miguel de Icaza <miguel@gnome.org>
2023-05-28 14:26:46 -0400

    Add

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftGodot",
+    products: [
+        .library(
+            name: "SwiftGodot",
+            targets: ["SwiftGodot"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SwiftGodot",
+            url: "https://github.com/migueldeicaza/SwiftGodot/releases/download/v0.9/SwiftGodot.xcframework.zip",
+            checksum: "1554ea2a6fafe32ff411258a187ec3ec588278010af84a3c8a1ebb216d339384")
+    ]
+)
