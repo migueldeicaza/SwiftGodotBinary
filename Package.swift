f1ed18dd2d70129870d170a9fbe54cc9dc8a5333
Miguel de Icaza <miguel@gnome.org>
2023-06-08 18:13:32 -0400

    Update to v0.14

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SwiftGodot",
-            url: "https://github.com/migueldeicaza/SwiftGodot/releases/download/v0.13/SwiftGodot.xcframework.zip",
+            url: "https://github.com/migueldeicaza/SwiftGodot/releases/download/v0.14/SwiftGodot.xcframework.zip",
             checksum: "1554ea2a6fafe32ff411258a187ec3ec588278010af84a3c8a1ebb216d339384")
     ]
 )
