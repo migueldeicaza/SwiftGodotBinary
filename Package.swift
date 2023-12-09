c5636f24eea2e17b18d0285dcc6af2a37742228a
Kyle <kyle201817146@gmail.com>
2023-12-09 14:22:25 +0800

    Add macOS CI to check the build

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,6 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "SwiftGodot",
             url: "https://github.com/migueldeicaza/SwiftGodot/releases/download/0.35.0/SwiftGodot.xcframework.zip",
-            checksum: "708457b8aa2096086cab612c2e87d97f3621874f73f1c53c7bd561d8c352969f")
+            checksum: "708457b8aa2096086cab612c2e87d97f3621874f73f1c53c7bd561d8c352969f"),
+        .testTarget(name: "SwiftGodotTests", dependencies: ["SwiftGodot"]),
     ]
 )
---
+++ Sources/SwiftGodotTests/SwiftGodotTests.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftGodotTests.swift
+//
+//
+//  Created by Kyle on 2023/12/13.
+//
+
+import SwiftGodot
+import XCTest
+
+final class SwiftGodotTests: XCTestCase {
+    func testExample() throws {}
+}
